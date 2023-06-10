6c13dc157fc0eb30fc92e674f9ef7ab01215b3cc
The-Flying-Dev <leandranm@outlook.com>
2023-06-11 01:52:11 +0200

    Q2

---
+++ 02_split_string_by_marker.rb
@@ -0,0 +1,30 @@
+# Split string by marker
+
+# Given a string str and another string marker, 
+# the method split_string should split the string str with marker and return the array that's generated.
+
+# Note: if value of marker is not provided, split_string should return str split by " ".
+
+
+def split_string(str, marker = nil)
+  # write your code here
+  str.split(marker)
+end
+
+
+str = "Michael Clarke Duncan"
+marker = " "
+
+puts split_string(str, marker)
+
+
+str = "Love, Death and Robots, "
+marker = ", "
+
+puts split_string(str, marker)
+
+
+str = "Haley Joel Osmont"
+marker = nil
+
+puts split_string(str, marker)
