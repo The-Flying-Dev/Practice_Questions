28467caa8e1b11995ce917b47818333b5effda38
The-Flying-Dev <leandranm@outlook.com>
2023-06-13 01:40:28 +0200

    Q24

---
+++ 24_list_all_cars_nested_hash.rb
@@ -0,0 +1,56 @@
+# List all cars made in the given region
+
+# manufacturers is an array of hashes, each hash representing a car manufacturer, 
+# with three keys "name", "region", and "cars". 
+# "cars" itself is an array of strings, each string representing a car model.
+
+# Given manufacturers and a region, the method should filter the manufacturers to only those 
+# that are made in the given region, and return the array of cars for each manufacturer.
+
+
+
+def cars_by_region(manufacturers, region)
+  # write your code here
+  manufacturers.select { |manufacturer| manufacturer["region"] == region }.map { |manufacturer| manufacturer["cars"] }
+end
+
+
+
+manufacturers = [
+  {
+    "name"=> "Ford",
+    "region"=> "USA",
+    "cars"=> ["Fusion", "EcoSport", "Explorer", "Ranger", "Edge", "Escape"],
+  },
+  {
+    "name"=> "Suzuki",
+    "region"=> "Asia",
+    "cars"=> ["Carry Truck", "Carry Dump Truck", "Jimny", "Landy"],
+  },
+  {
+    "name"=> "PSA Peugeot-Citroen",
+    "region"=> "Europe",
+    "cars"=> ["Peugeot 504", "Citroen GS", "Citroen CX", "Simca 1307-08", "Simca Horizon"],
+  },
+  {
+    "name"=> "Fiat-Chrysler",
+    "region"=> "Europe",
+    "cars"=> ["Alfa Romeo", "Pacifica", "Fiat 500X", "Fiat Egea"],
+  },
+  {
+    "name"=> "Honda",
+    "region"=> "Asia",
+    "cars"=> ["Civic", "Clarity", "CRV", "Freed", "S660", "Shuttle", "Legend"],
+  },
+  {
+    "name"=> "Jeep",
+    "region"=> "USA",
+    "cars"=> ["Waggoneer", "Wrangler", "Cherokee", "Compass", "Renegade", "Gladiator"],
+  },
+]
+region = "USA"
+
+
+
+
+p cars_by_region(manufacturers, region)
